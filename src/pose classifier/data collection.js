1875cada5a39810ba1c3cd4bbcb9e9717a46c35d
Dain Lee <65268003+dain-lee@users.noreply.github.com>
2020-10-06 02:18:03 +0900

    Add files via upload

---
+++ src/pose classifier/data collection.js
@@ -0,0 +1,103 @@
+// ml5.js: Pose Classification
+// The Coding Train / Daniel Shiffman
+// https://thecodingtrain.com/Courses/ml5-beginners-guide/7.2-pose-classification.html
+// https://youtu.be/FYgYyq-xqAw
+
+// All code: https://editor.p5js.org/codingtrain/sketches/JoZl-QRPK
+
+// Separated into three sketches
+// 1: Data Collection: https://editor.p5js.org/codingtrain/sketches/kTM0Gm-1q
+// 2: Model Training: https://editor.p5js.org/codingtrain/sketches/-Ywq20rM9
+// 3: Model Deployment: https://editor.p5js.org/codingtrain/sketches/c5sDNr8eM
+
+let video;
+let poseNet;
+let pose;
+let skeleton;
+
+let brain;
+
+let state = 'waiting';
+let targeLabel;
+
+function keyPressed() {
+ if (key == 's') {
+    brain.saveData();
+  } else {
+    targetLabel = key;
+    console.log(targetLabel);
+    setTimeout(function() {
+      console.log('collecting');
+      state = 'collecting';
+      setTimeout(function() {
+        console.log('not collecting');
+        state = 'waiting';
+      }, 10000);
+    }, 10000);
+  }
+}
+
+function setup() {
+  createCanvas(640, 480);
+  
+  video = createCapture(VIDEO);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+
+  let options = {
+    inputs: 34,
+    outputs: 4,
+    task: 'classification',
+    debug: true
+  }
+  brain = ml5.neuralNetwork(options);
+}
+
+function gotPoses(poses) {
+  // console.log(poses); 
+  if (poses.length > 0) {
+    pose = poses[0].pose;
+    skeleton = poses[0].skeleton;
+    if (state == 'collecting') {
+      let inputs = [];
+      for (let i = 0; i < pose.keypoints.length; i++) {
+        let x = pose.keypoints[i].position.x;
+        let y = pose.keypoints[i].position.y;
+        inputs.push(x);
+        inputs.push(y);
+      }
+      let target = [targetLabel];
+      brain.addData(inputs, target);
+    }
+  }
+}
+
+
+function modelLoaded() {
+  console.log('poseNet ready');
+}
+
+function draw() {
+  translate(video.width, 0);
+  scale(-1, 1);
+  image(video, 0, 0, video.width, video.height);
+
+  if (pose) {
+    for (let i = 0; i < skeleton.length; i++) {
+      let a = skeleton[i][0];
+      let b = skeleton[i][1];
+      strokeWeight(2);
+      stroke(0);
+
+      line(a.position.x, a.position.y, b.position.x, b.position.y);
+    }
+    for (let i = 0; i < pose.keypoints.length; i++) {
+      let x = pose.keypoints[i].position.x;
+      let y = pose.keypoints[i].position.y;
+      fill(0);
+      stroke(255);
+      ellipse(x, y, 16, 16);
+    }
+  }
+}
