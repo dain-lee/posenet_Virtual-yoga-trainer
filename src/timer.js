5fbc34f2237bfcb24907162a163c4c2319be97ff
Yunsun Hwang <70623875+sunniie@users.noreply.github.com>
2020-11-01 04:35:18 +0900

    Update timer.js

--- src/timer.js
+++ src/timer.js
@@ -3,6 +3,7 @@ var initialOffset = '440';
 var i = 5;
 var j = 1;
 var interval = setInterval(function () {
+<<<<<<< HEAD
         $('.circle_animation').css('stroke-dashoffset', initialOffset - (j * (initialOffset / 5)));
         $('.timer').text(i);
         if (i == 1) {
@@ -13,3 +14,14 @@ var interval = setInterval(function () {
         j++;
     }, 1000);
 
+=======
+    $('.circle_animation').css('stroke-dashoffset', initialOffset - (j * (initialOffset / 5)));
+    $('.timer').text(i);
+    if (i == 1) {
+        clearInterval(interval);
+        setTimeout("$('.timer').text('0')", 1000);
+    }
+    i--;
+    j++;
+}, 1000);
+>>>>>>> b277e29dbc1a7fdbbe4e45fafa7eca1874cca724
