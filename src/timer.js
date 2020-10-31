983fa5ba0b5dfb526668e0a899133f927cedf687
Dain Lee <65268003+dain-lee@users.noreply.github.com>
2020-11-01 04:51:25 +0900

    Update timer.js

--- src/timer.js
+++ src/timer.js
@@ -3,18 +3,6 @@ var initialOffset = '440';
 var i = 5;
 var j = 1;
 var interval = setInterval(function () {
-<<<<<<< HEAD
-        $('.circle_animation').css('stroke-dashoffset', initialOffset - (j * (initialOffset / 5)));
-        $('.timer').text(i);
-        if (i == 1) {
-            clearInterval(interval);
-            setTimeout("$('.timer').text('PASSED')", 1000);
-        }
-        i--;
-        j++;
-    }, 1000);
-
-=======
     $('.circle_animation').css('stroke-dashoffset', initialOffset - (j * (initialOffset / 5)));
     $('.timer').text(i);
     if (i == 1) {
@@ -24,4 +12,3 @@ var interval = setInterval(function () {
     i--;
     j++;
 }, 1000);
->>>>>>> b277e29dbc1a7fdbbe4e45fafa7eca1874cca724
